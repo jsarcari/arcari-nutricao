d60ef422e3b01727010cf6db816d542b34ef4e9b
Juan Arcari <juansantos1995@hotmail.com>
2020-09-30 18:39:59 -0300

    criaçao do template principal

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -1,9 +1,13 @@
-<h3>Pacientes</h3>
-<a href="{{ route ('pacientes.create')}}">Novo paciente</a>
-<ol>
-    @foreach ($pacientes as $p)
-        <li>
-            {{ $p['nome'] }}
-        </li>
-    @endforeach
-</ol>
+@extends('layouts.principal')
+
+@section('conteudo')
+    <h3>Pacientes</h3>
+    <a href="{{ route ('pacientes.create')}}">Novo paciente</a>
+    <ol>
+        @foreach ($pacientes as $p)
+            <li>
+                {{ $p['nome'] }}
+            </li>
+        @endforeach
+    </ol>
+@endsection
