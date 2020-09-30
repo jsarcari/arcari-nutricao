74a14c2020d0e0d4379af0f7d058c3bb466e2c6b
Juan Arcari <juansantos1995@hotmail.com>
2020-09-30 07:04:13 -0300

    Rename paciente's controller to PacienteController

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pacientes', 'Paciente@listarPacientes');
+Route::get('/pacientes', 'PacienteController@listarPacientes');
